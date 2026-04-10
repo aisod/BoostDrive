2ee2a0a7217a2ff182fc4a602a7ffa20edaa66ab
Saya Mubiana <mubianasaya@gmail.com>
2026-04-10 09:58:55 +0200

    ..

--- scripts/build_netlify.sh
+++ scripts/build_netlify.sh
@@ -29,7 +29,7 @@ echo "Using environment variables:"
 echo "SUPABASE_URL=${SUPABASE_URL}"
 echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}"
 
-flutter build web --release --web-renderer html \
+flutter build web --release \
   --dart-define=SUPABASE_URL="${SUPABASE_URL}" \
   --dart-define=SUPABASE_ANON_KEY="${SUPABASE_ANON_KEY}" \
   --dart-define=GOOGLE_MAPS_API_KEY="${GOOGLE_MAPS_API_KEY}"
